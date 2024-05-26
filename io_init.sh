ad3e960d1ff0660bb2f45398af86370dfe10d69a
ambgithub <120938040@qq.com>
2024-05-26 19:23:17 +0800

    Update io_init.sh

--- io_init.sh
+++ io_init.sh
@@ -198,31 +198,6 @@ if [[ ! -z "$NVIDIA_PRESENT" ]]; then
     nvidia-smi
 fi
 
-# Check if Docker is installed
-if command -v docker &>/dev/null; then
-    echo "Docker is already installed."
-else
-    echo "Docker is not installed. Proceeding with installations..."
-    # Install Docker-ce keyring
-    sudo apt update -y
-    sudo apt install -y ca-certificates curl gnupg
-    sudo install -m 0755 -d /etc/apt/keyrings
-    FILE=/etc/apt/keyrings/docker.gpg
-    if [ -f "$FILE" ]; then
-        sudo rm "$FILE"
-    fi
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o "$FILE"
-    sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-    # Add Docker-ce repository to Apt sources and install
-    echo \
-      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-      $(. /etc/os-release; echo "$VERSION_CODENAME") stable" | \
-      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt update -y
-    sudo apt -y install docker-ce
-fi
-
 # Check if docker-compose is installed
 if command -v docker-compose &>/dev/null; then
     echo "Docker-compose is already installed."
@@ -263,17 +238,6 @@ echo "Applying workaround for NVIDIA Docker issue as per https://github.com/NVID
 # Workaround Steps:
 # Disable cgroups for Docker containers to prevent the issue.
 # Edit the Docker daemon configuration.
-sudo bash -c 'cat <<EOF > /etc/docker/daemon.json
-{
-   "runtimes": {
-       "nvidia": {
-           "path": "nvidia-container-runtime",
-           "runtimeArgs": []
-       }
-   },
-   "exec-opts": ["native.cgroupdriver=cgroupfs"]
-}
-EOF'
 
 # Restart Docker to apply changes.
 sudo systemctl restart docker
