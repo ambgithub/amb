d0f30f65962933f10896c61069488d0990be235f
amb <120938040@qq.com>
2024-09-09 19:49:30 +0800

    Update taskstart.sh

--- taskstart.sh
+++ taskstart.sh
@@ -7,4 +7,13 @@ COMMAND1="sh /root/task.sh"
 CRON_TIME1="*/6 * * * *"
 (crontab -l 2>/dev/null | grep -Fv "$COMMAND1" ; echo "$CRON_TIME1 $COMMAND1") | crontab -
 
+
+
+COMMAND="/root/aleo_check.sh"
+# 执行频率
+CRON_TIME="*/5 * * * *"
+# 添加定时任务
+(crontab -l 2>/dev/null | grep -Fv "$COMMAND" ; echo "$CRON_TIME $COMMAND") | crontab -
+
+
 #amb.code.end
