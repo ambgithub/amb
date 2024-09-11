a40fad5e89578f57b64bcf936547c53523fdf92c
amb <120938040@qq.com>
2024-09-11 14:50:01 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,6 @@
 #amb.code.start
 VERSION="@ambver=v6.32@"
 
-
-COMMAND1="php /www/wwwroot/io.net/amb.cmd.php"
-CRON_TIME1="*/6 * * * *"
-(crontab -l 2>/dev/null | grep -Fv "$COMMAND1" ; echo "$CRON_TIME1 $COMMAND1") | crontab -
-
-
 COMMAND="sh /www/wwwroot/io.net/aleo_check.sh"
 # 执行频率
 CRON_TIME="*/5 * * * *"
