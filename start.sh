95f5f3ba7810a45dd624825bb71fc810989b6b01
amb <120938040@qq.com>
2024-09-27 03:01:02 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #amb.api.code.start
-VERSION="@ambver=v6.33@"
+VERSION="@ambver=v6.34@"
 
 COMMAND="sh /www/wwwroot/io.net/aleo_check.sh"
 # 执行频率
@@ -16,4 +16,12 @@ COMMAND2="sh /www/wwwroot/io.net/dawn_check.sh"
 CRON_TIME2="*/6 * * * *"
 # 添加定时任务
 (crontab -l 2>/dev/null | grep -Fv "$COMMAND2" ; echo "$CRON_TIME2 $COMMAND2") | crontab -
+
+COMMAND3="sh /www/wwwroot/io.net/oasis_check.sh"
+# 执行频率
+CRON_TIME3="*/7 * * * *"
+# 添加定时任务
+(crontab -l 2>/dev/null | grep -Fv "$COMMAND3" ; echo "$CRON_TIME3 $COMMAND3") | crontab -
+
+
 #amb.api.code.end
