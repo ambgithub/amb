1d9adcf44f993ad65fec5fdc0c957f51186492f1
amb <120938040@qq.com>
2024-10-13 05:47:57 +0800

    Update check_aireg.sh

--- check_aireg.sh
+++ check_aireg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #amb.api.code.start
-VERSION="@ambver=v7.1@"
+VERSION="@ambver=v7.2@"
 VERSION_API="https://io.ues.cn/host/api/checkshell?type=run_app&ver="
 DOWNLOAD_URL="https://raw.githubusercontent.com/ambgithub/amb/main/aireg"
 
