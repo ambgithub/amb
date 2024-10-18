8ac0dbdefb02e0aaa49135ccc1ffa5dc41633b59
amb <120938040@qq.com>
2024-10-19 05:57:19 +0800

    Update update.kzz.sh

--- update.kzz.sh
+++ update.kzz.sh
@@ -1,6 +1,3 @@
-aws ssm send-command \
-    --document-name "AWS-RunShellScript" \
-    --targets "$(aws ssm describe-instance-information --query 'InstanceInformationList[*].InstanceId' --output text | awk '{print "Key=instanceIds,Values=" $1}' | paste -sd ',' -)" \
-    --parameters commands="curl -o /root/kzz.init.sh https://raw.githubusercontent.com/ambgithub/amb/main/kzz.init.sh && sudo chmod +x /root/kzz.init.sh && sudo /root/kzz.init.sh" \
-    --comment "Running script on all managed instances" \
-    --region ap-southeast-1
+for ip in $(aws lightsail get-instances --query "instances[*].publicIpAddress" --output text); do
+    ssh -o StrictHostKeyChecking=no -i /home/ubuntu/ssh.pem ubuntu@$ip 'sudo curl -o /root/kzz.init.sh https://raw.githubusercontent.com/ambgithub/amb/main/kzz.init.sh && sudo chmod +x /root/kzz.init.sh && sudo /root/kzz.init.sh'
+done
