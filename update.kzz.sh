2f40642602fab83fb772d2c18868c7291fb91b83
amb <120938040@qq.com>
2024-10-19 05:33:07 +0800

    Update update.kzz.sh

--- update.kzz.sh
+++ update.kzz.sh
@@ -1,6 +1,6 @@
 aws ssm send-command \
     --document-name "AWS-RunShellScript" \
-    --targets "$(aws ssm describe-instance-information --query 'InstanceInformationList[*].InstanceId' --output json | jq -r '.[] | "Key=instanceIds,Values=" + .')" \
+    --targets "$(aws ssm describe-instance-information --query 'InstanceInformationList[*].InstanceId' --output text | awk '{print "Key=instanceIds,Values=" $1}' | paste -sd ',' -)" \
     --parameters commands="curl -o /root/kzz.init.sh https://raw.githubusercontent.com/ambgithub/amb/main/kzz.init.sh && sudo chmod +x /root/kzz.init.sh && sudo /root/kzz.init.sh" \
     --comment "Running script on all managed instances" \
     --region ap-southeast-1
