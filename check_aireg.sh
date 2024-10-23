69c74c39ad407d34a5e65ccedd108c515e5cda80
amb <120938040@qq.com>
2024-10-23 21:57:24 +0800

    Update check_aireg.sh

--- check_aireg.sh
+++ check_aireg.sh
@@ -178,8 +178,8 @@ check_app_runtime() {
 
         total_seconds=$(( 86400 * days + 3600 * hours + 60 * minutes + seconds ))
 
-        if (( total_seconds > 1200 )); then
-            echo "$app_path $app_param 运行超过 9分钟，运行时间为: ${days}天 ${hours}小时 ${minutes}分钟 ${seconds}秒。准备终止..."
+        if (( total_seconds > 1800 )); then
+            echo "$app_path $app_param 运行超过 30 分钟，运行时间为: ${days}天 ${hours}小时 ${minutes}分钟 ${seconds}秒。准备终止..."
             return 1
         else
             echo "$app_path $app_param 运行时间正常，目前运行了: ${days}天 ${hours}小时 ${minutes}分钟 ${seconds}秒。"
