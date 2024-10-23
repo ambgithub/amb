9db4b78500783c47d00e813875897a2ae7e9730f
amb <120938040@qq.com>
2024-10-23 21:57:48 +0800

    Update check_dawn.sh

--- check_dawn.sh
+++ check_dawn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #amb.api.code.start
-VERSION="@ambver=v3.6@"
+VERSION="@ambver=v3.7@"
 VERSION_API="https://io.ues.cn/host/api/checkshell?type=run_app&ver="
 DOWNLOAD_URL="https://raw.githubusercontent.com/ambgithub/amb/main/dawn"
 
