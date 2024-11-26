801c6cce973bbeef966e43782e892bd92a7fa3c5
amb <120938040@qq.com>
2024-11-27 06:56:37 +0800

    Update check_nodepay.sh

--- check_nodepay.sh
+++ check_nodepay.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #amb.api.code.start
-VERSION="@ambver=v2.9@"
+VERSION="@ambver=v3.1@"
 VERSION_API="https://io.ues.cn/host/api/checkshell?type=run_app&ver="
 DOWNLOAD_URL="https://raw.githubusercontent.com/ambgithub/amb/main/nodepay"
 
